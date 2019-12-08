658a3b0f826bc11627974dde501e250328bdb8c9
DUAN-SEAN <404064259@qq.com>
2019-12-08 13:02:31 +0800

    Add files via upload

---
+++ Common/CommonLib/Base/Log/ILog.cs
@@ -0,0 +1,12 @@
+﻿namespace Crazy.Common
+{
+    internal interface ILog
+    {
+        void Trace(string message);
+        void Warning(string message);
+        void Info(string message);
+        void Error(string v);
+        void Debug(string message);
+        void Fatal(string v);
+    }
+}
